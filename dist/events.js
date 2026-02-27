8ca9801eaf176d8e42c0a7756e447291a6011145
ckirschberg@gmail.com <ckirschberg@gmail.com>
2026-02-27 12:31:04 +0100

    template

--- dist/events.js
+++ dist/events.js
@@ -18,12 +18,7 @@ btn.addEventListener("mouseout", () => {
 });
 const input = document.getElementById("inputTodo");
 const btnAddTodo = document.getElementById("btnTodo");
-// waiting on page to load, and then run some js
-window.addEventListener("load", () => {
-    // let name = prompt("What is your name?");
-    // alert("Hello, " + name);
-});
-alert(new Date(2026, 14, 27));
+//alert(new Date(2026,14,27))
 btnAddTodo.addEventListener("click", () => {
     const todoValue = input.value; // retrieve user input
     console.log(new Date().toLocaleString());
@@ -69,5 +64,53 @@ document.getElementById("formUser")
     if (password.trim() === "") {
         addErrorLi("Password must be filled out");
     }
+});
+document.getElementById("btnJoke").addEventListener("click", async function () {
+    const url = 'https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist';
+    const alexanderSovOverSigOgDetErOk = await fetch(url, {
+        method: 'GET'
+    });
+    // Svarer serveren med en statuskode som er en fejl
+    if (!alexanderSovOverSigOgDetErOk.ok) {
+        alert("Noget gik galt");
+    }
+    // Konvertér fra json til javascript repræsentation
+    const data = await alexanderSovOverSigOgDetErOk.json();
+    console.log(data);
+});
+async function signup(myUsername, myPassword) {
+    const port = 8080;
+    const backendUrl = "http://localhost:" + port;
+    const response = await fetch(backendUrl, {
+        method: 'POST',
+        body: JSON.stringify({ username: myUsername, password: myPassword })
+    });
+    if (!response.ok) {
+        alert("Something went wrong signing up");
+    }
+    const data = await response.json();
+    console.log(data);
+}
+// Dummy data som bliver erstattet af et fetch kald til Python backend'en.
+async function getUsers() {
+    return [{ firstname: 'Mig', lastname: 'P', email: 'mai@mai.dk', password: 1234 },
+        { firstname: 'Anarikke', lastname: 'P', email: 'mafdsfadsfi@mai.dk', password: 2345 },
+        { firstname: 'Lea', lastname: 'P', email: 'dsfaddfsfa@mai.dk', password: 1237 },
+        { firstname: 'Laura', lastname: 'P', email: 'mfadsfdsafsadfaai@mai.dk', password: 1238 }
+    ];
+}
+// waiting on page to load, and then run some js
+window.addEventListener("load", async () => {
+    const users = await getUsers();
+    users.map((user) => {
+        const template = document.getElementById("template");
+        const kopi = template.content.cloneNode(true);
+        kopi.getElementById("name").textContent = user.firstname + " " + user.lastname;
+        kopi.getElementById("email").textContent = user.email + " " + user.email;
+        kopi.getElementById("password").textContent = user.password;
+        document.getElementById("userContainer").appendChild(kopi);
+    });
+    // let name = prompt("What is your name?");
+    // alert("Hello, " + name);
 });
 //# sourceMappingURL=events.js.map
