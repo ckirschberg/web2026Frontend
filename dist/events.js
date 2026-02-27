80eced51797f72b7fd4dfdb9ba458ba0887a3898
ckirschberg@gmail.com <ckirschberg@gmail.com>
2026-02-27 09:58:42 +0100

    validation

--- dist/events.js
+++ dist/events.js
@@ -18,15 +18,32 @@ btn.addEventListener("mouseout", function () {
 });
 var input = document.getElementById("inputTodo");
 var btnAddTodo = document.getElementById("btnTodo");
+// waiting on page to load, and then run some js
+window.addEventListener("load", function () {
+    // let name = prompt("What is your name?");
+    // alert("Hello, " + name);
+});
+alert(new Date(2026, 14, 27));
 btnAddTodo.addEventListener("click", function () {
     var todoValue = input.value; // retrieve user input
+    console.log(new Date().toLocaleString());
     // create a new li - element
     var li = document.createElement("li");
+    li.title = new Date().toUTCString();
     li.textContent = todoValue; // set its content to ...
     li.addEventListener("click", function () {
         this.remove(); // remove the li-element the user clicked
     });
     document.getElementById("todos").prepend(li); //add li to ul
     input.value = '';
+});
+document.getElementById("formUser")
+    .addEventListener("click", function (alexanderErISverige) {
+    alexanderErISverige.preventDefault(); // stops default form behaviour, to send a post request to server
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    console.log(firstname, lastname, email, password);
 });
 //# sourceMappingURL=events.js.map
--- src/events.ts
+++ src/events.ts
@@ -27,11 +27,21 @@ btn.addEventListener("mouseout", () => {
 const input: any = document.getElementById("inputTodo");
 const btnAddTodo: HTMLElement | null = document.getElementById("btnTodo");
 
+// waiting on page to load, and then run some js
+window.addEventListener("load", () => {
+    // let name = prompt("What is your name?");
+    // alert("Hello, " + name);
+});
+
+alert(new Date(2026,14,27))
+
 btnAddTodo.addEventListener("click", () => {
     const todoValue = input.value; // retrieve user input
     
+    console.log(new Date().toLocaleString());
     // create a new li - element
     const li = document.createElement("li");
+    li.title = new Date().toUTCString();
     li.textContent = todoValue; // set its content to ...
     li.addEventListener("click", function() { // add event listener to the new li element
         this.remove(); // remove the li-element the user clicked
@@ -39,4 +49,21 @@ btnAddTodo.addEventListener("click", () => {
 
     document.getElementById("todos").prepend(li); //add li to ul
     input.value = '';
+})
+
+document.getElementById("formUser")
+.addEventListener("click", (alexanderErISverige: Event) => {
+    alexanderErISverige.preventDefault(); // stops default form behaviour, to send a post request to server
+
+    const firstname = (document.getElementById("firstname") as HTMLInputElement).value;
+    const lastname = (document.getElementById("lastname") as HTMLInputElement).value;
+    const email = (document.getElementById("email") as HTMLInputElement).value;
+    const password = (document.getElementById("password") as HTMLInputElement).value;
+    
+    console.log(firstname, lastname, email, password);
+    
+    if (firstname === "") {
+        
+    }
+
 })
